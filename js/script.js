769ffc915144d6a7a82ddf76a852bbe6cb623ff2
Ash! <113056162+ashley-monaghan@users.noreply.github.com>
2023-01-09 13:56:33 -0500

    sync

--- js/script.js
+++ js/script.js
@@ -23,23 +23,15 @@ window.onload = function () {
   const params = new URLSearchParams(document.location.search)
 
   // input
-  const length = params.get("L")
+  const length = params.get("a")
   console.log(length)
-  const width = params.get("W")
-  console.log(width)
-  const height = params.get("H")
-  console.log(height)
 
   // process
-  const volume = (length * width * height) / 3
-  const dimension1 = "<ul>\n<li>r = " + length + "</li>\n</ul>"
-  const dimension2 = "<ul>\n<li>r = " + width + "</li>\n</ul>"
-  const dimension3 = "<ul>\n<li>r = " + height + "</li>\n</ul>"
+  const volume = length ** 3
+  const dimension = "<ul>\n<li>r = " + length + "</li>\n</ul>"
 
   // output
-  document.getElementById("dimension1").innerHTML = dimensions
-  document.getElementById("dimension2").innerHTML = dimensions
-  document.getElementById("dimension2").innerHTML = dimensions
+  document.getElementById("dimension").innerHTML = dimension
   document.getElementById("volume").innerHTML =
     "The volume is: " + volume.toFixed(2) + " cm³!"
 }
